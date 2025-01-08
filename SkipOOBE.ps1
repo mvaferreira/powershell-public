6877a98af14d81b6726932acd42bf0c3795295d1
Marcus Ferreira <maferre@microsoft.com>
2025-01-08 17:55:32 -0300

    updated

--- SkipOOBE.ps1
+++ SkipOOBE.ps1
@@ -1,6 +1,5 @@
 Write-Host "CUSTOM OOBE KEYS : Adding keys..."
-New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE -Name PrivacyConsentStatus -Value 1 -PropertyType DWORD -Force
-New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE -Name ProtectYourPC -Value 3 -PropertyType DWORD -Force
+New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE -Name PrivacyConsentStatus -Value 0 -PropertyType DWORD -Force
 New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE -Name HideEULAPage -Value 1 -PropertyType DWORD -Force
 New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE -Name HideOEMRegistrationScreen -Value 1 -PropertyType DWORD -Force
 New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE -Name HideWirelessSetupInOOBE -Value 1 -PropertyType DWORD -Force
