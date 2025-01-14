be6dfdbc796a354e4293369aa5a72edb307b07e8
Marcus Ferreira <maferre@microsoft.com>
2025-01-14 16:10:24 -0300

    updated SkipOOBE.ps1

--- SkipOOBE.ps1
+++ SkipOOBE.ps1
@@ -1,4 +1,6 @@
 Write-Host "CUSTOM OOBE KEYS FOR PRIVACY : Adding keys..."
+New-Item -Path HKLM:\Software\Policies\Microsoft\Windows\OOBE -Force
 New-ItemProperty -Path HKLM:\Software\Policies\Microsoft\Windows\OOBE -Name DisablePrivacyExperience -Value 1 -PropertyType DWORD -Force
+New-Item -Path HKLM:\Software\Policies\Microsoft\Windows\DataCollection -Force
 New-ItemProperty -Path HKLM:\Software\Policies\Microsoft\Windows\DataCollection -Name AllowTelemetry -Value 3 -PropertyType DWORD -Force
 Write-Host "CUSTOM OOBE KEYS FOR PRIVACY : Done."
