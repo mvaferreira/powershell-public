2fd940612c0e89a2d5cbbf35e6e8d78eff28efdb
Marcus Ferreira <maferre@microsoft.com>
2025-01-14 13:49:28 -0300

    updated SkipOOBE.ps1

--- SkipOOBE.ps1
+++ SkipOOBE.ps1
@@ -1,9 +1,4 @@
-Write-Host "CUSTOM OOBE KEYS : Adding keys..."
-New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE -Name PrivacyConsentStatus -Value 0 -PropertyType DWORD -Force
-New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE -Name HideEULAPage -Value 1 -PropertyType DWORD -Force
-New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE -Name HideOEMRegistrationScreen -Value 1 -PropertyType DWORD -Force
-New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE -Name HideWirelessSetupInOOBE -Value 1 -PropertyType DWORD -Force
-New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE -Name SkipMachineOOBE -Value 1 -PropertyType DWORD -Force
-New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE -Name SkipUserOOBE -Value 1 -PropertyType DWORD -Force
-New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE -Name ProtectYourPC -Value 3 -PropertyType DWORD -Force
-Write-Host "CUSTOM OOBE KEYS : Done."
+Write-Host "CUSTOM OOBE KEYS FOR PRIVACY : Adding keys..."
+New-ItemProperty -Path HKLM:\Software\Policies\Microsoft\Windows\OOBE -Name DisablePrivacyExperience -Value 1 -PropertyType DWORD -Force
+New-ItemProperty -Path HKLM:\Software\Policies\Microsoft\Windows\DataCollection -Name AllowTelemetry -Value 3 -PropertyType DWORD -Force
+Write-Host "CUSTOM OOBE KEYS FOR PRIVACY : Done."
