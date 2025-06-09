549e457f196455e1998f36dede0e2f61b1d84442
Marcus Ferreira <maferre@microsoft.com>
2025-06-09 18:51:17 -0300

    updated WindowsClearGhostedNICs.ps1

--- WindowsClearGhostedNICs.ps1
+++ WindowsClearGhostedNICs.ps1
@@ -1,6 +1,19 @@
 <#
+Disclaimer
+	The sample scripts are not supported under any Microsoft standard support program or service. 
+	The sample scripts are provided AS IS without warranty of any kind.
+	Microsoft further disclaims all implied warranties including, without limitation, any implied warranties of merchantability
+	or of fitness for a particular purpose.
+	The entire risk arising out of the use or performance of the sample scripts and documentation remains with you.
+	In no event shall Microsoft, its authors, or anyone else involved in the creation, production,
+	or delivery of the scripts be liable for any damages whatsoever (including, without limitation,
+	damages for loss of business profits, business interruption, loss of business information, or other pecuniary loss)
+	arising out of the use of or inability to use the sample scripts or documentation,
+	even if Microsoft has been advised of the possibility of such damages.
 .SYNOPSIS
     This script identifies and removes "ghosted" Mellanox (VEN_15B3) network interface cards (NICs) from the Windows registry.
+    Author: Marcus Ferreira marcus.ferreira[at]microsoft[dot]com
+    Version: 0.1
 .DESCRIPTION
     - Scans the registry for Mellanox NICs.
     - Compares registry entries to currently present network adapters.
